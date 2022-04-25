067547fb268f21871ee0aaff5229c0aad7688c3e
ThaisSBarbosa <thssantosbarbosa@gmail.com>
2022-04-24 22:40:14 -0300

    Refatorando...

--- src/main/java/com/mycompany/parserxmljson/Principal.java
+++ src/main/java/com/mycompany/parserxmljson/Principal.java
@@ -8,38 +8,27 @@ public class Principal {
     public static void main(String[] args) throws Exception {
 
         String inputXml = ExemploXML.inputXml;
-        int posInicioTag = 0;
+        int posicaoInicioChave = 0;
 
-        while (posInicioTag > -1) {
-            posInicioTag = inputXml.indexOf("<");
+        //enquanto houver uma nova chave, faz as substituições
+        while (posicaoInicioChave > -1) {
+            posicaoInicioChave = inputXml.indexOf("<");
 
-            if (posInicioTag > -1) {
-                int posFinalTag = inputXml.indexOf(">", posInicioTag);
-                String tag = inputXml.substring(posInicioTag + 1, posFinalTag);
+            if (posicaoInicioChave > -1) {
+                int posicaoFinalChave = inputXml.indexOf(">", posicaoInicioChave);
+                String chave = inputXml.substring(posicaoInicioChave + 1, posicaoFinalChave);
 
-                inputXml = inputXml.replaceAll("<" + tag + ">", "\"" + tag + "\":|");
-                inputXml = inputXml.replaceAll("</" + tag + ">", "||");
+                inputXml = marcaInicioEFinalValorDaChave(inputXml, chave);     
             } else {
-                break;
+                break;      //se não houver chave, sai do laço
             }
         }
 
-        Pattern p = Pattern.compile("[|][A-Za-z0-9. ][^:]*[||]");
-        Matcher m = p.matcher(inputXml);
-        while (m.find()) {
+        // regex para pegar valores entre | e ||
+        Matcher m = Pattern.compile("[|][A-Za-z0-9. ][^:]*[||]").matcher(inputXml);
+        while (m.find()) {            
             var valor = m.group().replace("||\n||", "}");
-
-            Pattern pValor = Pattern.compile("^[1-9]\\d*(\\.\\d+)?$");
-            Matcher mValor = pValor.matcher(valor.substring(1, valor.indexOf("||")));
-            if (mValor.find()) {
-                valor = valor.replace("||", ",");     //é número
-                valor = valor.replace("|", "");
-            } else {
-                valor = valor.replace("||", "\",");   //é texto
-                valor = valor.replace("|", "\"");
-            }
-
-            inputXml = inputXml.replace(m.group(), valor);
+            inputXml = ajustaValoresTextoOuNumericos(valor, inputXml, m);
         }
 
         inputXml = inputXml.replace(",\n\t,", "\n},");
@@ -48,6 +37,29 @@ public class Principal {
         inputXml = inputXml.replace(",\n||", "}");
 
         System.out.println(inputXml);
+    }
 
+    /// Marca início do valor com | e final com ||.
+    private static String marcaInicioEFinalValorDaChave(String inputXml, String chave) {
+        inputXml = inputXml.replaceAll("<" + chave + ">", "\"" + chave + "\":|");     //substitui por ex. <tag> por chave:|
+        inputXml = inputXml.replaceAll("</" + chave + ">", "||");                     //substitui por ex. </tag> por ||
+        return inputXml;
+    }
+
+    private static String ajustaValoresTextoOuNumericos(String valor, String inputXml, Matcher m) {
+        
+        // regex para pegar números inteiros e decimais
+        Matcher mValor = Pattern.compile("^[1-9]\\d*(\\.\\d+)?$").matcher(valor.substring(1, valor.indexOf("||")));
+        
+        if (mValor.find()) {
+            valor = valor.replace("||", ",");     //é número
+            valor = valor.replace("|", "");
+        } else {
+            valor = valor.replace("||", "\",");   //é texto
+            valor = valor.replace("|", "\"");
+        }
+        
+        inputXml = inputXml.replace(m.group(), valor);
+        return inputXml;
     }
 }
